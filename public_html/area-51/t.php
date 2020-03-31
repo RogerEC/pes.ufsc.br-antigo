92587f2b0191af806034719774172726c2b73509
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-03-31 14:29:48 -0300

    teste

--- public_html/area-51/t.php
+++ public_html/area-51/t.php
@@ -1,13 +1,14 @@
 <?php 
     $path = $_SERVER['DOCUMENT_ROOT'];
     require_once($path."/scripts/php/banco/conexao.php");
-    $aluno = $conexao->query("SELECT NOME, SOBRENOME, EMAIL FROM pes_info_pessoal WHERE TIPO_INFO = 'A'");
+    $aluno = $conexao->query("SELECT ID_ALUNO FROM pes_aluno");
     if(!empty($aluno)){
-        foreach($aluno as $a){
+        /*foreach($aluno as $a){
             $email = $a['EMAIL'];
             //$sobrenome = $a['SOBRENOME'];
             //$nome = $a['NOME'];
             echo "$email;";
-        }
+        }*/
+        echo $aluno->num_rows;
     }
 ?>

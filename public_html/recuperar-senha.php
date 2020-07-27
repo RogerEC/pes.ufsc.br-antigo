55433bf81de4712326560dfb7ff5b8df51cbf4a1
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-07-26 21:14:01 -0300

    -

--- public_html/recuperar-senha.php
+++ public_html/recuperar-senha.php
@@ -1,18 +1,3 @@
 <?php 
-    $path = $_SERVER['DOCUMENT_ROOT'];
-    require_once($path."/scripts/php/banco/conexao.php");
-
-    if(!empty($_POST["cpf"]) && !empty($_POST["tipo"])){
-
-        $cpf = $_POST["cpf"];
-        $cpf = preg_replace('/[^0-9]/', '', $cpf);
-        $tipo = $_POST["tipo"];
-
-        $resultado = $conexao->query("SELECT email FROM $tipo WHERE cpf = '{$cpf}' LIMIT 1");
-
-        $email = $resultado->fetch_row()[0];
-
-        echo $email;
-        
-    }
+    
 ?>

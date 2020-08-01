fe0b1873004ae2a3d46cf628d829c326a1a08a71
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-08-01 10:46:51 -0300

    Adicionado Rascunho de consultas úteis aos dados do banco

--- banco/Rascunhos_Consultas.sql
+++ banco/Rascunhos_Consultas.sql
@@ -1,4 +1,4 @@
--- primeira opção
+-- Primeira opção de matéria
 SELECT a.NOME, a.SOBRENOME, d.NOME  FROM pes_info_pessoal a, pes_volunt b, pes_disciplina d, pes_disciplina_prof c 
 WHERE b.VERSAO_PS = '2020-2' and b.ID_VOLUNT = a.ID_VOLUNT and c.ID_VOLUNT = a.ID_VOLUNT AND d.ID_DISCIPLINA = c.ID_DISCIPLINA and c.OPCAO = 1
 

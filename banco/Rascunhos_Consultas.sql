6ef2dc9b23b4ad7b29ae6d234ae6d4dd3493c854
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2020-12-16 23:07:35 -0300

    Alterada codificação nos scripts

--- banco/Rascunhos_Consultas.sql
+++ banco/Rascunhos_Consultas.sql
@@ -5,3 +5,12 @@ WHERE b.VERSAO_PS = '2020-2' and b.ID_VOLUNT = a.ID_VOLUNT and c.ID_VOLUNT = a.I
 -- Listar todas as opções
 SELECT a.NOME, a.SOBRENOME, d.NOME, c.OPCAO FROM pes_info_pessoal a, pes_volunt b, pes_disciplina d, pes_disciplina_prof c
 WHERE b.VERSAO_PS = '2020-2' and b.ID_VOLUNT = a.ID_VOLUNT and c.ID_VOLUNT = a.ID_VOLUNT AND d.ID_DISCIPLINA = c.ID_DISCIPLINA
+
+-- Listar todas as opções ordenando
+SELECT a.NOME, a.SOBRENOME, d.NOME, c.OPCAO FROM pes_info_pessoal a, pes_volunt b, pes_disciplina d, pes_disciplina_prof c
+WHERE b.VERSAO_PS = '2020-2' and b.ID_VOLUNT = a.ID_VOLUNT and c.ID_VOLUNT = a.ID_VOLUNT AND d.ID_DISCIPLINA = c.ID_DISCIPLINA
+ORDER BY a.NOME ASC, c.OPCAO ASC 
+
+-- Pegar Todas Info
+SELECT * FROM pes_volunt a, pes_info_pessoal b
+WHERE a.IS_VOLUNT = b.ID_VOLUNT AND a.VERSAO_PS = '2020-2' AND a.TIPO_VOLUNTARIO = 'P'
--- public_html/scripts/php/componentes/links-head.php
+++ public_html/scripts/php/componentes/links-head.php
@@ -1,4 +1,5 @@
-<meta charset="utf-8">
+<?php header('Content-Type: text/html; charset=UTF-8'); ?>
+        <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="theme-color" content="#681323">
         <meta name="author" content="Projeto Educação Solidária">
